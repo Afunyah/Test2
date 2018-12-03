a4f5c794ce10cc5f12f34d87f552d3d097c9aa35
Marcel Afunyah <marcelafunyah@gmail.com>
2018-12-03 22:54:47 +0000

    Success maybe hbs3

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.engine('hbs',hbs({extname:'hbs', defaultLayout: 'layout', layoutsDir:__dirname+'/views'}));
+app.engine('hbs');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
