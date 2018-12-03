9836e5ba685c8bacc833f43789f9f7ce7c6845b4
Marcel Afunyah <marcelafunyah@gmail.com>
2018-12-03 23:47:31 +0000

    Testing Proc5 www serve2 wit app param

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-wwwserve.wwwserve();
+wwwserve.wwwserve(app);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
